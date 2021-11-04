cee399bcea1d532935b936faa24694d0ba3f1d3e
George Kobaidze <george.kobaidze.14@gmail.com>
2021-11-04 20:48:11 +0400

    Add single liskov substitution principle example

---
+++ src/DesignPatterns/Solid/LiskovSubstitution/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.Solid.LiskovSubstitution
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle() { }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString() => $"Width: {Width}\nHeight: {Height}\nArea: {Width * Height}";
+    }
+}
