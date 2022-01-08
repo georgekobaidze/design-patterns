6f0cdfd8db0b8a183513ff5c354814acc5092a29
George Kobaidze <george.kobaidze.14@gmail.com>
2022-01-08 17:38:42 +0400

    Add composite example

---
+++ src/DesignPatterns/Structural/Composite/Composite/IGiftOperations.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatterns.Structural.Composite.Component;
+
+namespace DesignPatterns.Structural.Composite.Composite;
+
+// This is a helper interface that will be implemented by a composite.
+// Methods defined in this interface can be also defined in a component as abstract methods.
+// But instead we define them here because we shouldn't make leaf class implement those methods.
+public interface IGiftOperations
+{
+    void AddGift(Gift gift);
+    void RemoveGift(Gift gift);
+}
---
+++ src/DesignPatterns/Structural/Composite/Leaf/SingularGift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DesignPatterns.Structural.Composite.Component;
+
+namespace DesignPatterns.Structural.Composite.Leaf;
+
+public class SingularGift : Gift
+{
+    public SingularGift(string name, decimal price) 
+        : base(name, price)
+    {
+    }
+
+    public override decimal CalculateTotalCost()
+    {
+        Console.WriteLine($"{Name} with price {Price}");
+        return Price;
+    }
+}
